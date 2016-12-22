2859a14b032bc3f7974f3155074f330735ee7eaa
Jayvee-Air <jayveehe@gmail.com>
2016-12-22 11:08:19 +0800

    增加ubuntu的工作环境配置

---
+++ UnixConfigs/workspace_setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install vim git zsh tmux
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+mkdir ~/git_project
+cd ~/git_project
+git clone https://github.com/JayveeHe/JayveeTools.git
