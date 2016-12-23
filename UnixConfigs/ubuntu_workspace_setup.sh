bbb3471a649417ca6b743b0d0ac2e29e64f92dc2
Jayvee-Air <jayveehe@gmail.com>
2016-12-23 10:02:24 +0800

    rename workspace shell

--- UnixConfigs/ubuntu_workspace_setup.sh
+++ UnixConfigs/ubuntu_workspace_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# 进行ubuntu系统的个人工作环境初始化
 sudo apt-get install vim git zsh tmux
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 mkdir ~/git_project
